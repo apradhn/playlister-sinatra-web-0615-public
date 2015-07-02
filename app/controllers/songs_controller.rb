eb4d4f750c387b5be861280f0559b2f409280f77
apradhn <abhijit.pradhan6@gmail.com>
2015-07-02 15:37:29 -0400

    started building song forms

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,10 +5,38 @@ class SongsController < ApplicationController
     erb:'/songs/index'
   end
 
+  get '/songs/new' do 
+    @genres = Genre.all
+    erb :'/songs/new'
+  end  
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
+  end
+
   get '/songs/:slug' do
     slug = params[:slug]
     @song = Song.find_by_slug(slug)
     erb :'/songs/show'
+  end
+
+  post '/songs' do 
+    song = Song.new(params[:song])
+    song.artist = params[:artist]
+    song.genres < params[:genre]
+    song.save
+    redirect "/songs"
+  end
+
+  patch '/songs/:slug' do
+    binding.pry
+    song = Song.find_by_slug(params[:slug])
+    song = Song.new(params[:song])
+    song.artist = params[:artist]
+    song.genres << params[:genre]
+    song.save
+
   end
 
 end
